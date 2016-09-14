8cbaa996b771ebd8ab9f20e03add93243c51e100
Dave Wolf <dave45678@gmail.com>
2016-09-14 16:56:20 -0400

    initial commit

---
+++ src/Customer.java
@@ -0,0 +1,62 @@
+
+public class Customer {
+	private String fullName;
+	private String salutation;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String email;
+	private String position;
+	private String company;
+	
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getSalutation() {
+		return salutation;
+	}
+	public void setSalutation(String salutation) {
+		this.salutation = salutation;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+
+}
---
+++ src/Example.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Example {
+	public static void main(String[] args){
+		File file = new File("/home/oracle/workspace/LinkedListScannerTokenizerFileExample/src/customers.txt");
+		Scanner scan = null;
+		try {
+			scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("error, file not found");
+		}
+		scan.useDelimiter("\\t");
+		// Printing the delimiter used
+		System.out.println("The delimiter is "+scan.delimiter());
+		
+		//create a linkedList for our customers
+		LinkedList<Customer> customers = new LinkedList<Customer>();
+		
+		
+		//create a printWriter to write to the file
+		PrintWriter writer=null;
+		try {
+			writer = new PrintWriter(new File("output.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		
+		//skip the first line which contains headers
+		scan.nextLine();
+		for (int i=0;i<25;i++){
+			Customer c = new Customer();
+			c.setFullName(scan.next());
+			c.setSalutation(scan.next());
+			c.setFirstName(scan.next());
+			c.setLastName(scan.next());
+			c.setAddress(scan.next());
+			c.setEmail(scan.next());
+			c.setPosition(scan.next());
+			c.setCompany(scan.next());
+			scan.nextLine();
+			System.out.println(c.getFullName());
+			
+
+			//add customer to linkedList of customers
+			customers.add(c);
+			
+			//write customer and email address only to file if customer
+			writer.print(c.getFullName());
+			writer.println(c.getEmail());
+			
+		}
+		// closing the scanner stream
+		scan.close();
+		//close the print writer
+		writer.close();
+		
+	}
+
+}
